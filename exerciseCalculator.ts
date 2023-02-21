140f1f615c53344cb419f2d3e1337b8f28d6cade
Pyry Koivisto <pyry.koivisto@iki.fi>
2023-02-21 19:14:30 +0200

    Exercise 9.2

---
+++ exerciseCalculator.ts
@@ -0,0 +1,52 @@
+type Rating = 1 | 2 | 3;
+
+const rateResult = (average: number, target: number): Rating => {
+  if (average < 0.7 * target) {
+    return 1;
+  } else if (average < 1.3 * target) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
+
+const describeRating = (rating: Rating) => {
+  switch (rating) {
+    case 1:
+      return "Try harder!";
+    case 2:
+      return "A solid one!";
+    case 3:
+      return "Good job!";
+  }
+}
+
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: Rating
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (hourCounts: Array<number>, target: number): Result => {
+  const sum = hourCounts.reduce((acc, val) => acc + val, 0);
+  const periodLength = hourCounts.length;
+  const trainingDays = hourCounts.filter((hours) => hours > 0).length;
+  const average = periodLength > 0 ? sum / periodLength : 0;
+  const rating = rateResult(average, target);
+
+  return {
+    periodLength,
+    trainingDays,
+    success: average < target ? false : true,
+    rating,
+    ratingDescription: describeRating(rating),
+    target,
+    average
+  }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
