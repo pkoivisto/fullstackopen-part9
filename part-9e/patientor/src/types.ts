9271a22fa7b38b199228485479d7c8716352c8b3
Pyry Koivisto <pyry.koivisto@iki.fi>
2023-03-22 23:01:08 +0200

    Exercise 9.23: Copy type definitions for Entry from backend to frontend

--- part-9e/patientor/src/types.ts
+++ part-9e/patientor/src/types.ts
@@ -10,6 +10,55 @@ export enum Gender {
   Other = "other"
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge?: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+
+export type Entry =
+  | HealthCheckEntry
+  | HospitalEntry
+  | OccupationalHealthcareEntry;
+
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,6 +66,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Array<Entry>;
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
