98c7384aa99ce860c6d5e032161d6d524473e4fa
Pyry Koivisto <pyry.koivisto@iki.fi>
2023-03-26 19:02:04 +0300

    Exercise 9.25: Render full details for each entry type on patient's details page.

--- part-9e/patientor/src/components/PatientDetailsPage/index.tsx
+++ part-9e/patientor/src/components/PatientDetailsPage/index.tsx
@@ -4,29 +4,114 @@ import { Box } from "@mui/system";
 
 import patientService from "../../services/patients";
 import diagnosisService from "../../services/diagnoses";
-import { Patient, Entry, Diagnosis } from "../../types";
+import {
+  Patient,
+  Entry,
+  HospitalEntry,
+  Diagnosis,
+  HealthCheckEntry,
+  OccupationalHealthcareEntry,
+} from "../../types";
 import { Typography } from "@mui/material";
+
+type Diagnoses = Array<Diagnosis>;
+
+const HealthCheckEntryDetails = ({ entry }: { entry: HealthCheckEntry }) => {
+  return (
+    <div>
+      <b>Risk level</b> (0-3, lower is better): {entry.healthCheckRating}
+    </div>
+  );
+};
+
+const HospitalEntryDetails = ({ entry }: { entry: HospitalEntry }) => {
+  if (entry.discharge) {
+    return (
+      <div>
+        <b>Discharged</b> on {entry.discharge.date}. Cause:{" "}
+        {entry.discharge.criteria}.
+      </div>
+    );
+  } else {
+    return <div>Patient not discharged yet or discharge not recorded.</div>;
+  }
+};
+
+const OccupationalHealthcareEntryDetails = ({
+  entry,
+}: {
+  entry: OccupationalHealthcareEntry;
+}) => {
+  return (
+    <div>
+      <b>Employer</b>: {entry.employerName}
+      <br />
+      {entry.sickLeave ? (
+        <>
+          <b>Sick leave granted</b>: {entry.sickLeave.startDate} -{" "}
+          {entry.sickLeave.endDate}
+        </>
+      ) : (
+        <b>No sick leave granted.</b>
+      )}
+    </div>
+  );
+};
+
+const getEntryType = (entry: Entry) => {
+  switch (entry.type) {
+    case "HealthCheck":
+      return "Health check";
+    case "Hospital":
+      return "Hospital visit";
+    case "OccupationalHealthcare":
+      return "Occupational health care appointment";
+  }
+};
+
+const getEntryDetailsByType = (entry: Entry) => {
+  switch (entry.type) {
+    case "HealthCheck":
+      return <HealthCheckEntryDetails entry={entry} />;
+    case "Hospital":
+      return <HospitalEntryDetails entry={entry} />;
+    case "OccupationalHealthcare":
+      return <OccupationalHealthcareEntryDetails entry={entry} />;
+  }
+};
 
 const EntryDetails = ({
   entry,
   diagnoses,
 }: {
   entry: Entry;
-  diagnoses: Array<Diagnosis>;
+  diagnoses: Diagnoses;
 }) => {
   return (
-    <Typography variant="body1">
-      <>
-        {entry.date} <i>{entry.description}</i>
-      </>
-      <ul>
-        {entry.diagnosisCodes?.map((code, i) => (
-          <li key={i}>
-            {code} {diagnoses.find((d) => d.code === code)?.name ?? ''}
-          </li>
-        ))}
-      </ul>
-    </Typography>
+    <div style={{ padding: "4px", border: "2px dashed" }}>
+      <i>{getEntryType(entry)}</i> on {entry.date}
+      <br />
+      <i>{entry.description}</i>
+      {getEntryDetailsByType(entry)}
+      {entry.diagnosisCodes ? (
+        <>
+          <b>Diagnoses</b>:
+          <ul>
+            {entry.diagnosisCodes?.map((code, i) => (
+              <li key={i}>
+                {code} {diagnoses.find((d) => d.code === code)?.name ?? ""}
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <>
+          <b>No recorded diagnoses.</b>
+          <br />
+        </>
+      )}
+      <b>Attending specialist</b>: {entry.specialist}
+    </div>
   );
 };
 
@@ -35,15 +120,21 @@ const Entries = ({
   diagnoses,
 }: {
   patient: Patient;
-  diagnoses: Array<Diagnosis>;
+  diagnoses: Diagnoses;
 }) => {
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexFlow: "column",
+        gap: "4px",
+      }}
+    >
       <Typography variant="h4">Entries</Typography>
       {patient.entries.map((entry) => (
         <EntryDetails key={entry.id} entry={entry} diagnoses={diagnoses} />
       ))}
-    </>
+    </div>
   );
 };
 
@@ -52,7 +143,7 @@ const RenderPatientDetails = ({
   diagnoses,
 }: {
   patient: Patient;
-  diagnoses: Array<Diagnosis>;
+  diagnoses: Diagnoses;
 }) => {
   const PatientAttribute = ({ attribute }: { attribute: keyof Patient }) => {
     return (
@@ -79,7 +170,7 @@ const RenderPatientDetails = ({
 export const PatientDetailsPage = () => {
   const { patientId } = useParams();
   const [patientDetails, setPatientDetails] = useState<Patient | null>(null);
-  const [diagnoses, setDiagnoses] = useState<Array<Diagnosis>>([]);
+  const [diagnoses, setDiagnoses] = useState<Diagnoses>([]);
 
   useEffect(() => {
     if (patientId) {
